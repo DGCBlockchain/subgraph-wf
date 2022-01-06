90ef5ace4d7bb80f834b940920dc2fba0d482ce5
Muhammad Rameez <muhammad.rameez@dgc.org>
2022-01-06 19:40:43 +0100

    fixed reverse mapping from user->projects

--- src/Project.ts
+++ src/Project.ts
@@ -12,7 +12,7 @@ import { USER_ADDED } from '../generated/templates/Project/Project';
 export const FACTORY_ADDRESS = '0xdaaC03F38D08f8EaC92A51705C11FF1515c4A0BE';
 
 // TODO: Goal: I should be able to query the functionality of the following functions 
-// getAllProjects() - returns a list of all projects created from the factory
+// getAllProjects() - returns a list of all projects created from the factory -> Done
 // getUserProjects(userID) - returns all  projects a user is registered to along with quotas and other useful information 
 // getProjectUsers(projectID) - returns all users of a project with their quotas
 // getProjectDistributors(projectID) - returns all distributers that have distribution rights for a project
@@ -37,7 +37,8 @@ export function handleNewUser(event: USER_ADDED): void {
         userQuota.paid = BigDecimal.zero();
         userQuota.outstandingShare = BigDecimal.zero(); 
         userQuota.quota = event.params.quota;
-        userQuota.user = event.params.id.toString();       
+        // userQuota.user = event.params.id.toString();  
+        userQuota.user = user.id;   
     }
     userQuota.save();
     user.save();
@@ -65,7 +66,7 @@ export function handleNewUser(event: USER_ADDED): void {
   }
 }
 *********************************
-// get users
+// get users info
 {
   users {
     id
@@ -73,9 +74,60 @@ export function handleNewUser(event: USER_ADDED): void {
     organization
     quotas {
       id
-      user {
+      outstandingShare
+      project {
         id
+        name
       }
+    }
+  }
+}
+
+// get projects
+{
+    projects {
+    revenue
+    name
+    projectId
+    members {
+      id
+      quota
+      user {
+        name
+        organization
+      }
+    }
+  }
+}
+
+// query for a single project
+
+
+{
+    project(id: "0x5c671eaf3a6191b38b8052ab1b6db33b1714d6ec") {
+    id
+    revenue
+    name
+    projectId
+    members {
+      id
+      quota
+    }
+  }
+}
+
+// 
+{
+    project(id: "0xc0d9d25778a61270b6062c4f96d1cdb024758b42") {
+    id
+    revenue
+    name
+    projectId
+    members (where : {
+      id: "0x893017f5bff4951f7f893422767048a2bd6d0771-0xc0d9d25778a61270b6062c4f96d1cdb024758b42"
+    }) {
+      id
+      quota
     }
   }
 }
